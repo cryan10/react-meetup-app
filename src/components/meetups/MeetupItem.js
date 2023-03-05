f2a118f771aabfd982acf5fbbba567f0cdd6d6ba
Carrie <cryan1205@gmail.com>
2023-03-05 14:45:56 -0500

    adds work so far

---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,28 @@
+import Card from '../../ui/Card';
+import classes from './MeetupItem.module.css';
+
+function MeetupItem(props) {
+	return (
+		<li className={classes.item}>
+			<Card>
+				<div>
+					<img
+						src={props.image}
+						alt={props.title}
+						className={classes.image}
+					/>
+				</div>
+				<div className={classes.content}>
+					<h3>{props.title}</h3>
+					<address>{props.address}</address>
+					<p>{props.description}</p>
+				</div>
+				<div className={classes.actions}>
+					<button>Add to Favorites</button>
+				</div>
+			</Card>
+		</li>
+	);
+}
+
+export default MeetupItem;
